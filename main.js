d8380586b6d093621b5306d3b92467032526ade7
Erlliam <tommejas@gmail.com>
2020-06-23 02:37:35 -0400

    Parse hash, get user ID

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
+const clientID = 'u837pub66ryla2sfkh19iqp6rudq44';
+const getUsersURL = 'https://api.twitch.tv/helix/users';
+
+function parseHash(rawHash) {
+  rawHash = rawHash.substring(1);
+  return Object.fromEntries(rawHash.split('&').map(v => v.split('=')));
+}
+
 let rawHash = decodeURIComponent(document.location.hash);
-rawHash = rawHash.substring(1);
 
-let hash = Object.fromEntries(rawHash.split('&').map(v => v.split('=')));
+if (!(rawHash === '')) {
+  let hash = parseHash(rawHash);
 
-// GET https://api.twitch.tv/helix/users
-// The response has a JSON payload with a data field
-// containing an array of user-information elements.
+  if (hash.hasOwnProperty('access_token')) {
+    (async () => {
+      let accessToken = hash.access_token;
 
-(async () => {
-  let response = await fetch('https://api.twitch.tv/helix/users', {
+      let userID = await getUserID(accessToken);
+      if (userID === -1) {
+        console.log('Errors have happeneed');
+        return;
+      }
+      
+      if (isSubscribed(userID)) {
+        console.log('PogChamp');
+      }
+
+    })();
+  }
+}
+
+async function getUserID(accessToken) {
+  let response = await fetch(getUsersURL, {
     headers: {
-      Authorization: `Bearer ${hash.access_token}`
+      'Client-ID': clientID,
+      'Authorization': `Bearer ${accessToken}`
     }
   });
-  console.log(response)
 
-})();
+  if (response.status == 200) {
+    let rawJson = await response.json();
+    let json = rawJson.data[0];
+    return json.id;
+  }
+
+  return -1;
+}
+
+function isSubscribed(userID) {
+  // TODO
+  return true;
+}
+
